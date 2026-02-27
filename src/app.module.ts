9f4980e3a3ad71c466a113e58f4fbdd016bc824a
brandon Toapanta <brandontoapanta12345@gmail.com>
2026-02-26 23:42:28 -0500

    feat: configurar el módulo de configuración global en AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SriModule } from './sri/sri.module';
 
 @Module({
-  imports: [SriModule],
+  imports: [SriModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),],
   controllers: [AppController],
   providers: [AppService],
 })
